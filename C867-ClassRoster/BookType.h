e04b63dcc53f9b23b6fe8da41ea5b03e35a66a01
Korinne Stegall <69525188+KorinneStegall@users.noreply.github.com>
2023-06-28 14:40:20 -0500

    Update BookType.h

--- C867-ClassRoster/BookType.h
+++ C867-ClassRoster/BookType.h
@@ -4,5 +4,3 @@ enum BookType { UNDECIDED, FICTION, NONFICTION};
 
 /*Parallel array pops out a string for each book type - use the book type as an index*/
 static const std::string bookTypeStrings[] = { "UNDECIDED", "FICTION", "NONFICTION" };
-
-//enum class DegreeProgram {SECURITY = 0, NETWORK = 1, SOFTWARE = 2};
